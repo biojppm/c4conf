355f4c7abfa87a70057d716da835918c29c9cb91
Joao Paulo Magalhaes <dev@jpmag.dev>
2021-06-24 12:15:15 +0100

    [wip] adding single configs

--- src/c4/conf/conf.hpp
+++ src/c4/conf/conf.hpp
@@ -8,8 +8,9 @@
 namespace c4 {
 namespace conf {
 
-/** When loading multiple files, an object of this class may be
- * provided as a workspace to allow reuse. */
+/** Provide a workspace tree to load newer files to merge.
+ * When loading multiple files, an object of this class may be
+ * provided as a workspace to allow reuse of the tree.. */
 struct Workspace
 {
     yml::Tree   m_tree_ws;
@@ -23,6 +24,7 @@ struct Workspace
 
     bool load_file(csubstr s, yml::Tree *output)
     {
+        C4_ASSERT(output);
         size_t pos = output->arena_pos();
         // filesystem functions use libc functions which cannot
         // deal with substrings so create a cstr
@@ -45,7 +47,7 @@ struct Workspace
     void prepare(It begin, It end, yml::Tree *output) const
     {
         // we have no guarantees that the filenames
-        // are zero-terminated, as (regrettably) required 
+        // are zero-terminated, as (regrettably) required
         // by filesystem functions.
         size_t fns = 0; // max filename size
         for(It it = begin; it != end; ++it)
@@ -56,33 +58,56 @@ struct Workspace
         output->reserve_arena(fns + 1);
         substr arena = output->m_arena;
         C4_ASSERT(output->m_arena.len >= fns + 1);
-
         size_t count = 0;
-        for( ; begin != end; ++begin)
+        for(It it = begin; it != end; ++it)
         {
-            csubstr filename = to_csubstr(*begin);
+            csubstr filename = to_csubstr(*it);
             count += filename.len + 1;
             c4::cat(arena, filename, '\0');
             count += fs::file_size(arena.str);
         }
         output->reserve_arena(count);
     }
+
+
 };
 
 
 //-----------------------------------------------------------------------------
 
+/** load the given @p file_path and merge with the existing @p tree,
+    using an existing @p workspace */
+inline bool add_file(csubstr file_path, yml::Tree *tree, Workspace *workspace)
+{
+    C4_ASSERT(workspace != nullptr);
+    if( ! workspace->load_file(file_path, tree))
+        return false;
+    size_t root = tree->root_id();
+    tree->merge_with(workspace->m_tree, workspace->m_tree->root_id(), root);
+    return true;
+}
+
+/** load the given @p file_path and merge with the existing @p tree,
+    instantiating a temporary @p workspace */
+inline bool add_file(csubstr file_path, yml::Tree *tree)
+{
+    Workspace ws;
+    return add_file(file_path, tree, &ws);
+}
+
+
+//-----------------------------------------------------------------------------
+
+/** load a sequence of files */
 template <class It>
-inline bool load_range(It begin, It end, yml::Tree *tree, Workspace *ws=nullptr)
+inline bool add_files(It begin, It end, yml::Tree *tree, Workspace *ws=nullptr)
 {
     if(ws)
     {
         ws->prepare(begin, end, tree);
         bool ret = false;
-        for( ; begin != end; ++begin)
-        {
-            ret |= load(c4::to_csubstr(*begin), tree, ws);
-        }
+        for(It it = begin; it != end; ++it)
+            ret |= add_file(c4::to_csubstr(*it), tree, ws);
         return ret;
     }
     else
@@ -90,60 +115,34 @@ inline bool load_range(It begin, It end, yml::Tree *tree, Workspace *ws=nullptr)
         Workspace tmp;
         tmp.prepare(begin, end, tree);
         bool ret = false;
-        for( ; begin != end; ++begin)
-        {
-            ret |= load(c4::to_csubstr(*begin), tree, &tmp);
-        }
+        for(It it = begin; it != end; ++it)
+            ret |= add_file(c4::to_csubstr(*it), tree, &tmp);
         return ret;
     }
 }
 
+/** load a sequence of files, with the names given in a container with multiple filenames */
 template <class Container>
-inline bool load(Container const& cont, yml::Tree *tree, Workspace *ws=nullptr)
+inline bool add_files(Container const& container, yml::Tree *tree, Workspace *ws=nullptr)
 {
-    return load_range(cont.begin(), cont.end(), tree, ws);
+    return add_files(container.begin(), container.end(), tree, ws);
 }
 
 
 //-----------------------------------------------------------------------------
 
-inline bool load(csubstr path, yml::Tree *tree, Workspace *ws)
+inline bool add_conf(csubstr key, csubstr val, yml::Tree *tree, Workspace *ws)
 {
     C4_ASSERT(ws != nullptr);
-    if( ! ws->load_file(path, tree)) return false;
-    size_t root = tree->root_id();
-    tree->merge_with(ws->m_tree, ws->m_tree->root_id(), root);
+    C4_ASSERT(ws->m_tree != nullptr);
+    ws->m_tree->clear();
+    yml::parse(key, val, ws->m_tree);
+    auto lookup_result = tree->lookup_path(key);
+    if(!lookup_result)
+        return false;
+    C4_CHECK(lookup_result == true);
+    tree->merge_with(ws->m_tree, ws->m_tree->root_id(), lookup_result.target);
     return true;
-}
-
-
-//-----------------------------------------------------------------------------
-
-inline bool load(csubstr path, yml::Tree *tree)
-{
-    Workspace ws;
-    return load(path, tree, &ws);
-}
-
-
-//-----------------------------------------------------------------------------
-
-inline yml::Tree load(csubstr path, Workspace *ws)
-{
-    yml::Tree t;
-    load(path, &t, ws);
-    return t;
-}
-
-
-//-----------------------------------------------------------------------------
-
-inline yml::Tree load(csubstr path)
-{
-    yml::Tree t;
-    Workspace ws;
-    load(path, &t, &ws);
-    return t;
 }
 
 
--- test/basic.cpp
+++ test/basic.cpp
@@ -6,35 +6,42 @@
 #include <vector>
 
 
-using MultipleContents = std::initializer_list<c4::csubstr>;
+struct Conf
+{
+    c4::csubstr key, val;
+};
+using MultipleConfsSpec = std::initializer_list<Conf>;
+using MultipleFilesSpec = std::initializer_list<c4::csubstr>;
+
 struct MultipleFiles
 {
     std::vector<c4::fs::ScopedTmpFile> m_files;
     std::vector<c4::csubstr> m_filenames;
 
-    MultipleFiles(MultipleContents contents)
+    MultipleFiles(MultipleFilesSpec contents)
         : m_files(contents.begin(), contents.end()),
           m_filenames()
     {
         m_filenames.reserve(m_files.size());
         for(auto const& f : m_files)
-        {
             m_filenames.emplace_back(c4::to_csubstr(f.name()));
-        }
     }
 };
 
 
-void test_same(MultipleContents contents, c4::csubstr expected_in)
+void test_same(MultipleFilesSpec files, MultipleConfsSpec confs, c4::csubstr expected_in)
 {
-    MultipleFiles mf(contents);
-    ASSERT_EQ(mf.m_files.size(), contents.size());
-    ASSERT_EQ(mf.m_filenames.size(), contents.size());
+    MultipleFiles mf(files);
+    ASSERT_EQ(mf.m_files.size(), files.size());
+    ASSERT_EQ(mf.m_filenames.size(), files.size());
 
     c4::yml::Tree tree_result, tree_expected;
-    bool got_one = c4::conf::load(mf.m_filenames, &tree_result);
+    bool got_one = c4::conf::add_files(mf.m_filenames, &tree_result);
     EXPECT_TRUE(got_one);
     c4::yml::parse(expected_in, &tree_expected);
+    c4::conf::Workspace ws;
+    for(auto const& conf : confs)
+        c4::conf::add_conf(conf.key, conf.val, &tree_result, &ws);
 
     auto result = c4::yml::emitrs<std::string>(tree_result);
     auto expected = c4::yml::emitrs<std::string>(tree_expected);
@@ -48,6 +55,7 @@ TEST(basic, orthogonal)
 {
     test_same(
         {"a: 0", "b: 1", "c: 2"},
+        {},
         "{a: 0, b: 1, c: 2}"
     );
 }
@@ -56,6 +64,7 @@ TEST(basic, overriding0)
 {
     test_same(
         {"a: 0", "a: 1"},
+        {},
         "a: 1"
     );
 }
@@ -64,6 +73,7 @@ TEST(basic, overriding1)
 {
     test_same(
         {"a: 0", "a: 1", "a: 2"},
+        {},
         "a: 2"
     );
 }
@@ -72,6 +82,7 @@ TEST(basic, overriding2)
 {
     test_same(
         {"a: 0", "b: 1", "a: 2"},
+        {},
         "{a: 2, b: 1}"
     );
 }
@@ -80,6 +91,16 @@ TEST(basic, overriding3)
 {
     test_same(
         {"a: 0", "{a: 1, b: 1}", "c: 2"},
+        {},
         "{a: 1, b: 1, c: 2}"
+    );
+}
+
+TEST(basic, overriding_with_conf)
+{
+    test_same(
+        {"a: 0", "{a: 1, b: 1}", "c: 2"},
+        {{"a", "3"}, {"a", "4"}, {"d", "5"}},
+        "{a: 4, b: 1, c: 2, d: 5}"
     );
 }
